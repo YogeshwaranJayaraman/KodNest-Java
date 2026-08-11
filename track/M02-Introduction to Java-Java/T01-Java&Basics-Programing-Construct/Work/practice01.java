17dbd5ea27eb2146dd34679d334556cbacdcfe9c
YogeshwaranJayaraman <yogeshwaran15082005@gmail.com>
2026-08-11 07:47:57 +0530

    Name changed

---
+++ track/M02-Introduction to Java-Java/T01-Java&Basics-Programing-Construct/Work/practice01.java
@@ -0,0 +1,19 @@
+public class practice01 {
+    public static void main(String [] args)
+    {
+        int num=-25;
+        if (num>0){
+            System.out.println("Positive");
+        }
+        else if (num<0){
+            System.out.println("Negative");
+        }
+        else{ 
+             System.out.println("zero");
+        }
+        
+        
+        
+    }
+    
+}
