e4e120cb93ec57370a394c9dc2c7f1a278bdf79d
YogeshwaranJayaraman <yogeshwaran15082005@gmail.com>
2026-08-19 22:47:12 +0530

    3D Array

--- track/M02-Introduction to Java-Java/T08-2D-3D&Jagged-Array/ThreeDArray.java
+++ track/M02-Introduction to Java-Java/T08-2D-3D&Jagged-Array/ThreeDArray.java
@@ -1,4 +1,26 @@
 import java.util.Scanner;
 public class ThreeDArray {
-    
+    public static void main(String[] args) {
+        Scanner doc=new Scanner(System.in);
+        int arr[][][]=new int[2][2][5];
+        System.out.println("Enter the Element in the Array: ");
+        for(int i=0;i<=arr.length-1;i++) {
+            for (int j=0;j<=arr[i].length-1;j++) {
+                for(int k=0;k<=arr[i][j].length-1;k++) {
+                    arr[i][j][k]=doc.nextInt();
+                }
+            }
+        }
+        System.out.println("Array Element are: ");
+        for(int i=0;i<=arr.length-1;i++) {
+            for (int j=0;j<=arr[i].length-1;j++) {
+                for(int k=0;k<=arr[i][j].length-1;k++) {
+                    System.out.print(arr[i][j][k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+    }  
 }
